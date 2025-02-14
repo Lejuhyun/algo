413e9259498eada72bed6ed46b7dc64467cf6ec7
juhyunlee <ljh01099014032@gmail.com>
2025-02-14 16:29:37 +0900

    change

--- 배열뒤집기/sol2.py
+++ 배열뒤집기/sol2.py
@@ -1,8 +1,14 @@
 def solution(num_list):
     answer = num_list
+
+    temp_list = [0,0,0,0,0]
+
+
     length=len(num_list)
     for i in range(length):
-        num_list[i] = num_list[length-1-i]
-    return answer
+        print(length-1-i)
+        temp_list[i] = num_list[length-1-i]
+    
+    return temp_list
 
 print(solution([1, 2, 3, 4, 5]))
